1267b71c1cd2b3b752dc34b275d1c19af6226350
smasauko <bsc-fc-42-23@unima.ac.mw>
2026-02-02 11:12:52 +0200

    added staff componets

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import ExecutiveMembers from "./components/ExecutiveMembers"
 import Faqs from "./components/FAQs"
 import Footer from "./components/Footer"
 import Auth from "./pages/Auth"
+import Staff from "./components/Staff"
 
 export default function App() {
   return (
@@ -14,6 +15,7 @@ export default function App() {
       <Hero />
       <About />
       <Announcements />
+      <Staff/>
       <ExecutiveMembers />
       <Faqs />
       <Footer />
